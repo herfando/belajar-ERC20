a3aac5615c7140e3af14273f63eef17762ffd1f9
Herfando <herfando_91@yahoo.com>
2024-09-29 14:33:52 +0700

    Add files via upload

---
+++ contracts/TokenErc.sol
@@ -0,0 +1,50 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.27;
+
+contract TokenErc {
+    // variable
+    string public name = "Token ERc";
+    string public symbol = "TERC";
+    uint8 public decimals = 18; //default adalah 18
+    uint256 public totalSupply = 1000000 * 10 * uint8(decimals);
+
+    mapping(address => uint256) public balanceOf;
+    mapping(address => mapping(address => uint256)) public allowance;
+
+    event Transfer(address indexed from, address indexed to, uint256 value);
+    event Approval(address indexed owner, address indexed spender, uint256 value);
+
+
+    constructor(){
+        balanceOf[msg.sender] = totalSupply;
+    }
+
+    function transfer(address _to, uint256 _value) public returns (bool success) {
+        require(_to != address(0), "ERC20: transfer to the zero address");
+        require(balanceOf[msg.sender] >= _value, "Balance tidak mencukupi");
+
+        balanceOf[msg.sender] -= _value;
+        balanceOf[_to] += _value;
+        emit Transfer(msg.sender, _to, _value);
+        return true;
+    }
+
+    function approve(address _spender, uint256 _value) public returns (bool success) {
+        allowance[msg.sender][_spender] = _value;
+        emit Approval(msg.sender, _spender, _value);
+        return true;
+    }
+
+    function transferFrom(address _from, address _to, uint256 _value) public returns (bool success){
+        require(_from != address(0), "ERC20: transfer from the zero address");
+        require(_to != address(0), "ERC20: transfer to the zero address");
+        require(balanceOf[_from] >= _value, "Balance tidak mencukupi");
+        require(allowance[_from] [msg.sender] >= _value, "Tidak di izinkan");
+
+        balanceOf[_from] -= _value;
+        balanceOf[_to] += _value;
+        allowance[_from][msg.sender] -= _value;
+        emit Transfer(_from, _to, _value);
+        return true;
+    }
+}
